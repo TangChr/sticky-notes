a476ae35a7bdbc4219fba824479d7afd65ac4661
Christian Tang <git@tcdev.dk>
2017-01-11 23:28:25 +0100

    gulp-header & gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    header = require('gulp-header'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass');
+
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> v<%= pkg.version %>',
+  ' * Copyright (c) '+new Date().getFullYear()+' <%= pkg.author %>',
+  ' * <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
  
 gulp.task('sass', function () {
-  return gulp.src('./sticky.scss')
+  return gulp.src('./master.scss')
     .pipe(sass({style: 'expanded'}))
     .pipe(sass().on('error', sass.logError))
+    .pipe(rename('sticky.css'))
+    .pipe(header(banner, {pkg : pkg }))
     .pipe(gulp.dest('./dist/'));
 });
 
