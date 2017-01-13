248559a07d809db43a8fbfcd45268445130ac8ff
Christian Tang <git@tcdev.dk>
2017-01-14 00:51:58 +0100

    Removed repo from package.json and moved year to header options in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),
 var pkg = require('./package.json');
 var banner = ['/**',
   ' * <%= pkg.name %> v<%= pkg.version %>',
-  ' * Copyright (c) '+new Date().getFullYear()+' <%= pkg.author %>',
+  ' * Copyright (c) <%= year  %> <%= pkg.author %>',
   ' * <%= pkg.license %>',
   ' */',
   ''].join('\n');
@@ -16,7 +16,7 @@ gulp.task('sass', function () {
     .pipe(sass({style: 'expanded'}))
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('sticky.css'))
-    .pipe(header(banner, {pkg : pkg }))
+    .pipe(header(banner, {pkg: pkg, year: new Date().getFullYear()}))
     .pipe(gulp.dest('./dist/'));
 });
 
