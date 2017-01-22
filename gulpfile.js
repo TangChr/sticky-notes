7b2b2c31177350c0e1842b6c82134999a4422628
Christian Tang <git@tcdev.dk>
2017-01-22 23:27:38 +0100

    Add gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp   = require('gulp'),
+    clean  = require('gulp-clean-css'),
     header = require('gulp-header'),
     rename = require('gulp-rename'),
     sass   = require('gulp-sass'),
@@ -10,14 +11,23 @@ var banner = ['/**',
   ' * Licensed under <%= pkg.license %> (https://github.com/TangChr/sticky-notes/blob/master/LICENSE)',
   ' */',
   ''].join('\n');
+  var header_opts = {pkg: pkg, year: new Date().getFullYear()};
  
 gulp.task('sass', function () {
   return gulp.src('./master.scss')
     .pipe(sass({style: 'expanded'}))
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('sticky.css'))
-    .pipe(header(banner, {pkg: pkg, year: new Date().getFullYear()}))
+    .pipe(header(banner, header_opts))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('clean', ['sass'], function () {
+  return gulp.src('./dist/sticky.css')
+    .pipe(clean({compatibility: 'ie8'}))
+    .pipe(header(banner, header_opts))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['sass', 'clean']);
